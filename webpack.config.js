b804832f3a391192437258a7bc3d7f0a22eaa81f
panlazy <ucanberkcan@gmail.com>
2021-02-24 09:31:29 +0300

    ✨ Beautify

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,27 @@ const { join, resolve } = require("path");
 const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
-	entry: join(__dirname, "src", "index.ts"),
-	target: "node",
-	mode: process.env.NODE_ENV,
-	output: {
-		path: resolve(__dirname, "dist"),
-		filename: "bundle.js",
-	},
-	watch: process.env.NODE_ENV === "development",
-	resolve: {
-		extensions: [".ts", ".js"],
-	},
-	externals: [nodeExternals()],
-	module: {
-		rules: [
-			{
-				test: /\.ts$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "ts-loader",
-				},
-			},
-		],
-	},
+  entry: join(__dirname, "src", "index.ts"),
+  target: "node",
+  mode: process.env.NODE_ENV,
+  output: {
+    path: resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  watch: process.env.NODE_ENV === "development",
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+        },
+      },
+    ],
+  },
 };
